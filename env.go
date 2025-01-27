ddb38d5f6d5c629429805cb2686fd9b95c997229
yuanrui.1023 <yuanrui.1023@bytedance.com>
2025-01-27 17:37:38 +0800

    feat: 修改环境变量为 runtime_env

--- env.go
+++ env.go
@@ -7,10 +7,9 @@ const ENV_TEST = "test"
 const ENV_PROD = "prod"
 
 func ENV() string {
-	runtimeEnv := os.Getenv("RUNTIME_ENV")
+	runtimeEnv := os.Getenv("runtime_env")
 	if runtimeEnv == "" {
 		runtimeEnv = ENV_DEV
 	}
 	return runtimeEnv
 }
-
